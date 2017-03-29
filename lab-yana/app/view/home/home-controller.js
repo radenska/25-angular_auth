c0c782a843de1d28ad521b6791e72c7177dbf2ce
radenska <yana_radenska@yahoo.com>
2017-03-29 14:48:51 -0700

    created gallery service and create gallery controller, both functional, need styling

--- lab-yana/app/view/home/home-controller.js
+++ lab-yana/app/view/home/home-controller.js
@@ -2,8 +2,20 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', HomeController];
+module.exports = ['$log', '$rootScope', 'galleryService', HomeController];
 
-function HomeController($log) {
+function HomeController($log, $rootScope, galleryService) {
   $log.debug('HomeController');
+
+  this.galleries = [];
+
+  this.fetchGalleries = function() {
+    $log.debug('HomeController.fetchGalleries');
+
+    galleryService.fetchGalleries()
+    .then(galleries => this.galleries = galleries);
+  };
+
+  this.fetchGalleries();
+  $rootScope.$on('$locationChangeSuccess', () => this.fetchGalleries());
 }
