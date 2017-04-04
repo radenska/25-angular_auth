190f53f4ed794b1f5ae3306aada3ce94c1042436
radenska <yana_radenska@yahoo.com>
2017-04-04 14:13:44 -0700

    added auth get token and create gallery tests

---
+++ lab-yana/test/auth-service-test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Auth Service', function() {
+  beforeEach(() => {
+    angular.mock.module('cfgram');
+    angular.mock.inject(($rootScope, authService, $window, $httpBackend) => {
+      this.$rootScope = $rootScope;
+      this.authService = authService;
+      this.$httpBackend = $httpBackend;
+      this.$window = $window;
+    });
+  });
+  describe('authService.getToken', () => {
+    it('should return a token', () => {
+      this.authService.token = null;
+      this.$window.localStorage.setItem('token', 'test token');
+      this.authService.getToken()
+      .then(token => expect(token).toEqual('test token'))
+      .catch(err => expect(err).toEqual(null));
+      this.$rootScope.$apply();
+    });
+  });
+});
