5b780de2b3f3e3a8c9fe49b1a0e1b596a2010eae
radenska <yana_radenska@yahoo.com>
2017-04-11 14:12:56 -0700

    added travis.yml and server.js files

---
+++ lab-yana/server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(`${__dirname}/build`));
+app.listen(PORT, () => 'server up:', PORT);
